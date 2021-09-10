5fa174366be666ba986fee3643ee37a2f298dfc9
Yi-Wei Chen <feberium@gmail.com>
2021-09-10 18:17:44 -0500

    Add frame extraction primitive

--- autovideo/utils/d3m_utils.py
+++ autovideo/utils/d3m_utils.py
@@ -68,59 +68,65 @@ def build_pipeline(config):
     pipeline_description = Pipeline()
     pipeline_description.add_input(name='inputs')
 
-    #Step 0: Denormalise
-    step_0 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.autovideo.common.denormalize'))
+    #Step 0: Extract frames by extension
+    step_0 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.autovideo.common.extract_frames'))
     step_0.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='inputs.0')
     step_0.add_output('produce')
     pipeline_description.add_step(step_0)
 
-    #Step 1: Dataset to DataFrame
-    step_1 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.autovideo.common.dataset_to_dataframe'))
-    step_1.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce')
+    #Step 1: Denormalise
+    step_1 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.autovideo.common.denormalize'))
+    step_1.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='inputs.0')
     step_1.add_output('produce')
     pipeline_description.add_step(step_1)
 
-    #Step 2: Column Parser
-    step_2 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.autovideo.common.column_parser'))
+    #Step 2: Dataset to DataFrame
+    step_2 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.autovideo.common.dataset_to_dataframe'))
     step_2.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
     step_2.add_output('produce')
     pipeline_description.add_step(step_2)
 
-    #Step 3: Extract columns by semantic types - Attributes
-    step_3 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.autovideo.common.extract_columns_by_semantic_types'))
+    #Step 3: Column Parser
+    step_3 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.autovideo.common.column_parser'))
     step_3.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.2.produce')
     step_3.add_output('produce')
-    step_3.add_hyperparameter(name='semantic_types', argument_type=ArgumentType.VALUE,
-                                      data=['https://metadata.datadrivendiscovery.org/types/Attribute'])
     pipeline_description.add_step(step_3)
 
-    #Step 4: Extract Columns by semantic types - Target
+    #Step 4: Extract columns by semantic types - Attributes
     step_4 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.autovideo.common.extract_columns_by_semantic_types'))
-    step_4.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
+    step_4.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
     step_4.add_output('produce')
     step_4.add_hyperparameter(name='semantic_types', argument_type=ArgumentType.VALUE,
-                                      data=['https://metadata.datadrivendiscovery.org/types/TrueTarget'])
+                                      data=['https://metadata.datadrivendiscovery.org/types/Attribute'])
     pipeline_description.add_step(step_4)
 
-    #Step 5: Video primitive
-    algorithm = config.pop('algorithm', None)
-    alg_python_path = 'd3m.primitives.autovideo.recognition.' + algorithm
-    step_5 = PrimitiveStep(primitive=index.get_primitive(alg_python_path))
-    step_5.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
-    step_5.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.4.produce')
-    # Add hyperparameters
-    for key, value in config.items():
-        step_5.add_hyperparameter(name=key, argument_type=ArgumentType.VALUE, data=value)
+    #Step 5: Extract Columns by semantic types - Target
+    step_5 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.autovideo.common.extract_columns_by_semantic_types'))
+    step_5.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.2.produce')
     step_5.add_output('produce')
+    step_5.add_hyperparameter(name='semantic_types', argument_type=ArgumentType.VALUE,
+                                      data=['https://metadata.datadrivendiscovery.org/types/TrueTarget'])
     pipeline_description.add_step(step_5)
 
-    #Step 6: Construct the predictions
-    step_6 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.autovideo.common.construct_predictions'))
-    step_6.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.5.produce')
-    step_6.add_argument(name='reference', argument_type=ArgumentType.CONTAINER, data_reference='steps.2.produce')
+    #Step 6: Video primitive
+    algorithm = config.pop('algorithm', None)
+    alg_python_path = 'd3m.primitives.autovideo.recognition.' + algorithm
+    step_6 = PrimitiveStep(primitive=index.get_primitive(alg_python_path))
+    step_6.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.4.produce')
+    step_6.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.5.produce')
+    # Add hyperparameters
+    for key, value in config.items():
+        step_6.add_hyperparameter(name=key, argument_type=ArgumentType.VALUE, data=value)
     step_6.add_output('produce')
-    step_6.add_hyperparameter(name = 'use_columns', argument_type=ArgumentType.VALUE, data = [0,1])
     pipeline_description.add_step(step_6)
+
+    #Step 7: Construct the predictions
+    step_7 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.autovideo.common.construct_predictions'))
+    step_7.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.6.produce')
+    step_7.add_argument(name='reference', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
+    step_7.add_output('produce')
+    step_7.add_hyperparameter(name = 'use_columns', argument_type=ArgumentType.VALUE, data = [0,1])
+    pipeline_description.add_step(step_7)
 
     # Final Output
     pipeline_description.add_output(name='output predictions', data_reference='steps.6.produce')
