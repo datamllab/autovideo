a807bb9e69a7c702b9e940ae38c2e16e65deee8a
Daochen Zha <daochen.zha@tamu.edu>
2023-06-22 08:40:19 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -54,7 +54,6 @@ setup(
                      ]
     },
     classifiers=[
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
